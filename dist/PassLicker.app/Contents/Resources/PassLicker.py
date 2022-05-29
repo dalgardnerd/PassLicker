95996b97ad4f4e87a552a78f2c3734039556c557
dalgardnerd <41878198+dalgardnerd@users.noreply.github.com>
2022-05-29 09:34:14 -0600

    mac app

--- dist/PassLicker.app/Contents/Resources/PassLicker.py
+++ dist/PassLicker.app/Contents/Resources/PassLicker.py
@@ -6,18 +6,16 @@ root = Tk()
 root.title("PassLicker")
 root.resizable(False, False)
 root.configure(background='#08243b')
-root.iconbitmap("passlicker.ico")
+root.iconbitmap("passlicker.png")
 
 # global passphrase
 # global link
-
 
 def pushit_click():
     print(include_digits_value.get())
     passphrase = pw.get_passphrase(
         (int(num_chars_box.get())), int(total_words_box.get()), (word_separator_box.get()), include_digits_value.get())
-    link = pw.push_passphrase(passphrase, int(
-        expire_days_box.get()), int(expire_views_box.get()))
+    link = pw.push_passphrase(passphrase, int(expire_days_box.get()), int(expire_views_box.get()))
     text_box.configure(state='normal')
     text_box.insert(1.0, link)
     text_box.insert(1.0, '\n')
@@ -27,7 +25,6 @@ def pushit_click():
 
 # def copy_pass_click():
 #     print(passphrase)
-
 
 # define text box
 text_box = Text(root, height=20, width=80, padx=15, pady=15)
@@ -40,15 +37,9 @@ text_box.configure(state='disabled')
 
 # WIDGETS
 
-label_width = 15
-label_padx = 2
-label_pady = 10
-
-l0 = Label(root, height=1, bg='#08243b')
-l0.grid(column=0, row=0)
-
-l10 = Label(root, height=1, bg='#08243b')
-l10.grid(column=0, row=6)
+label_width=15
+label_padx=2
+label_pady=10
 
 num_chars_label = Label(root, text="Characters per word:",
                         width=label_width,
@@ -58,7 +49,7 @@ num_chars_label = Label(root, text="Characters per word:",
                         anchor='e',
                         padx=label_padx,
                         pady=label_pady)
-
+                        
 total_words_label = Label(root, text="Total words:",
                           width=label_width,
                           bg='#08243b',
@@ -84,35 +75,35 @@ include_digits_label = Label(root, text="Include Digits?",
                              padx=label_padx,
                              pady=label_pady)
 expire_days_label = Label(root, text="Expiration Days:",
-                          bg='#08243b',
+                             bg='#08243b',
+                             fg='white',
+                             border=2,
+                             anchor='e',
+                             padx=label_padx,
+                             pady=label_pady)
+expire_views_label = Label(root, text="Expiration Views:",
+                             bg='#08243b',
+                             fg='white',
+                             border=2,
+                             anchor='e',
+                             padx=label_padx,
+                             pady=label_pady)
+passphrase_settings_label = Label(root, text="Passphrase Settings:",
+                        width=label_width,
+                        bg='#1567ab',
+                        fg='white',
+                        border=2,
+                        anchor='e',
+                        padx=5,
+                        pady=5)
+                        
+link_settings_label = Label(root, text="Link Settings:",
+                          bg='#1567ab',
                           fg='white',
                           border=2,
                           anchor='e',
-                          padx=label_padx,
-                          pady=label_pady)
-expire_views_label = Label(root, text="Expiration Views:",
-                           bg='#08243b',
-                           fg='white',
-                           border=2,
-                           anchor='e',
-                           padx=label_padx,
-                           pady=label_pady)
-passphrase_settings_label = Label(root, text="Passphrase Settings:",
-                                  width=label_width,
-                                  bg='#1567ab',
-                                  fg='white',
-                                  border=2,
-                                  anchor='e',
-                                  padx=5,
-                                  pady=5)
-
-link_settings_label = Label(root, text="Link Settings:",
-                            bg='#1567ab',
-                            fg='white',
-                            border=2,
-                            anchor='e',
-                            padx=5,
-                            pady=5)
+                          padx=5,
+                          pady=5)
 
 num_chars_box = Spinbox(
     root,
@@ -128,7 +119,7 @@ total_words_box = Spinbox(
     width=2,
     border=2,
     wrap=True)
-expire_days_value = StringVar()
+expire_days_value= StringVar()
 expire_days_value.set("14")
 expire_days_box = Spinbox(
     root,
@@ -138,7 +129,7 @@ expire_days_box = Spinbox(
     width=2,
     border=2,
     wrap=True)
-expire_views_value = StringVar()
+expire_views_value= StringVar()
 expire_views_value.set("10")
 expire_views_box = Spinbox(
     root,
@@ -161,11 +152,11 @@ include_digits_box.select()
 include_digits_box.include_digits_value = include_digits_value
 
 
-pushit_button = Button(root, text="Push it", padx=20, pady=20, bg='white', fg="#17568a", font="Helvetica",
+pushit_button = Button(root, text="Push it", padx=20, pady=20,bg='white',fg="#17568a", font="Helvetica", 
                        command=pushit_click)
-# copy_pass_button = Button(root, text="Copy Passphrase", padx=0, pady=0,bg='white',fg="#17568a", font="Helvetica",
+# copy_pass_button = Button(root, text="Copy Passphrase", padx=0, pady=0,bg='white',fg="#17568a", font="Helvetica", 
 #                        command=copy_pass_click)
-# copy_link_button = Button(root, text="Copy Link", padx=20, pady=0,bg='#17568a',fg="#17568a", font="Helvetica",
+# copy_link_button = Button(root, text="Copy Link", padx=20, pady=0,bg='#17568a',fg="#17568a", font="Helvetica", 
 #                        command=pushit_click)
 
 # GRID
@@ -178,25 +169,25 @@ pushit_button.grid(column=2, row=0,  columnspan=1,
 # copy_link_button.grid(column=3, row=5,  columnspan=3,
 #                     rowspan=1, padx=10, pady=10)
 
-passphrase_settings_label.grid(column=0, row=1, columnspan=2)
-num_chars_box.grid(column=1, row=2)
-total_words_box.grid(column=1, row=3)
-word_separator_box.grid(column=1, row=4)
-include_digits_box.grid(column=1, row=5)
-expire_days_box.grid(column=4, row=3)
-expire_views_box.grid(column=4, row=4)
+passphrase_settings_label.grid(column=0, row=0,columnspan=2)
+num_chars_box.grid(column=1, row=1)
+total_words_box.grid(column=1, row=2)
+word_separator_box.grid(column=1, row=3)
+include_digits_box.grid(column=1, row=4)
+expire_days_box.grid(column=4, row=2)
+expire_views_box.grid(column=4, row=3)
 
 
-num_chars_label.grid(column=0, row=2)
-total_words_label.grid(column=0, row=3)
-word_separator_label.grid(column=0, row=4)
-include_digits_label.grid(column=0, row=5)
+num_chars_label.grid(column=0, row=1)
+total_words_label.grid(column=0, row=2)
+word_separator_label.grid(column=0, row=3)
+include_digits_label.grid(column=0, row=4)
 
-link_settings_label.grid(column=3, row=1, columnspan=2)
-expire_days_label.grid(column=3, row=3)
-expire_views_label.grid(column=3, row=4)
+link_settings_label.grid(column=3, row=0, columnspan=2)
+expire_days_label.grid(column=3, row=2)
+expire_views_label.grid(column=3, row=3)
 
 
-text_box.grid(column=0, row=7, columnspan=5)
+text_box.grid(column=0, row=6, columnspan=5)
 
 root.mainloop()
